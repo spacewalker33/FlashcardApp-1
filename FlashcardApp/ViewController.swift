a263e7fa2eca5133954fba4ccba79f8ccf3fd663
Simon Gomez <simongomez636@gmail.com>
2021-03-25 09:05:19 -0500

    Initial Commit

---
+++ FlashcardApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FlashcardApp
+//
+//  Created by Gomez, Simon on 3/25/21.
+//  Copyright © 2021 Gomez, Simon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
